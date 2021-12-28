98f2b7f1100809a6bb386767caae0e1f6bba4566
katytught <yu.000423@139.com>
2021-12-28 14:35:15 +0800

    keli

---
+++ keli/ATK TRUE/OBJ/asciilib.d
@@ -0,0 +1,5 @@
+..\obj\asciilib.o: AsciiLib.c
+..\obj\asciilib.o: AsciiLib.h
+..\obj\asciilib.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+..\obj\asciilib.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\inttypes.h
+..\obj\asciilib.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
